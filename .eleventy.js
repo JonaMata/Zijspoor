a146380fbe47381042054c53c68f6554ca56a3d1
Jonathan Matarazzi <git@jonathanm.nl>
2022-06-30 01:17:13 +0200

    Switched to netlifyCMS

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("media");
     return {
         dir: {
             layouts: "_layouts",
