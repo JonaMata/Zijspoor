f9c6b508cb3c9a142e95fc8a0aea81a837a4dd63
Jonathan Matarazzi <git@jonathanm.nl>
2022-08-01 20:48:53 +0200

    Added robots.txt to passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("media");
+    eleventyConfig.addPassthroughCopy("robots.txt");
     return {
         dir: {
             layouts: "_layouts",
